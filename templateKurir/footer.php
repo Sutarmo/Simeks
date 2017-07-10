2dd78498f6c613aed40f03c89ce72f5d5d9a30eb
Sutarmo <sutarmo.sisfountan@gmail.com>
2017-07-10 16:59:08 +0700

    Sutarmo

---
+++ templateKurir/footer.php
@@ -0,0 +1,22 @@
+<footer>
+    <div id="footer">
+        <div class="row">
+            <div class="col-md-12 col-xs-12">
+              <p  align="center" >@CopyRight Sistem Informasi 2015 </p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+ 
+    <script src="../assets/js/jquery.min.js"></script>
+    <script src="../assets/js/sbootstrap.min.js"></script>
+    <script src="../assets/js/metisMenu.min.js"></script>
+    <script src="../assets/js/sb-admin-2.js"></script>
+    <script>
+       
+    </script>
+
+</body>
+
+</html>
---
+++ templateKurir/sidebar.php
@@ -0,0 +1,14 @@
+<div id="wrapper">
+       
+            <div class="sidebar"  >
+            <div id="profil" style="min-height: 501px">
+           <img src="../images/administrator-256.png" height="100px" >Nama : Abdinal           
+             <ul class="nav nav-pills nav-stacked" id="navi">
+                    <li role="presentation" ><a href="kurir.html" class="active"><button type="button" class="btn btn-primary btn-lg btn-block">Profil</button></a></li>
+                    <li role="presentation" ><a href="satuspengirimankurir.html"><button type="button" class="btn btn-primary btn-lg btn-block">Status Pengiriman</button></a></li>
+                    
+                </ul>
+
+            </div>
+
+                          </div>
